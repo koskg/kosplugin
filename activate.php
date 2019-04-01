6ea47d3d5845bca8c2a2e8d5bd45e3b1d363387e
koskg <k.a.anpilov@gmail.com>
2019-04-01 13:13:17 +0600

    test

---
+++ activate.php
@@ -0,0 +1,4 @@
+<?php
+
+OW::getNavigation()->addMenuItem(OW_Navigation::MAIN, 'kosplugin-index', 'kosplugin', '#####', OW_Navigation::VISIBLE_FOR_MEMBER);
+
---
+++ init.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Defined routes for plugin kosplugin
+ */
+OW::getRouter()->addRoute(new OW_Route('kosplugin-index', 'kosplugin', 'kosplugin_CTRL_Example', 'index'));
+
