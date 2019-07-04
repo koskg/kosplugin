983bee7d7826cc75d1e12d52c47fd2484f77474c
koskg <k.a.anpilov@gmail.com>
2019-07-04 17:57:40 +0600

    teat index

--- controllers/test.php
+++ controllers/test.php
@@ -18,12 +18,36 @@ class KOSPLUGIN_CTRL_Test extends OW_ActionController
     public function index()
     {
         $language = OW::getLanguage();
-        //$router = OW::getRouter();
+        $router = OW::getRouter();
 
         OW::getDocument()->addStyleSheet( OW::getPluginManager()->getPlugin('kosplugin')->getStaticCssUrl().'kosplugin.css' );
 
         OW::getDocument()->setTitle($language->text("kosplugin", "index_page_title"));
-        OW::getDocument()->setDescription($language->text("kosplugin", "index_page_description"));
+        OW::getDocument()->setDescription($language->text("kosplugin", "index_page_description"));  //Спросить
         OW::getDocument()->setHeading($language->text("kosplugin", "index_page_heading"));
+
+
+        $testMenu = array();
+
+        $testMenu[] = array(
+            "label" => "Form",
+            "url" => $router->urlFor("KOSPLUGIN_CTRL_Test", "form")
+        );
+
+        $testMenu[] = array(
+            "label" => "22222",
+            "url" => $router->urlFor("KOSPLUGIN_CTRL_Test", "index")
+        );
+        $this->assign("menu", $testMenu);
+
     }
+
+    public function form()
+    {
+        OW::getDocument()->setTitle("Form");
+        OW::getDocument()->setHeading("Заговок");
+
+    }
+
+
 }
