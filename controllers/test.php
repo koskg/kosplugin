36a8cab5dc6d904558eb417af52a9d5daf443337
koskg <k.a.anpilov@gmail.com>
2019-07-04 16:05:49 +0600

    css

--- controllers/test.php
+++ controllers/test.php
@@ -9,14 +9,21 @@
 
 class KOSPLUGIN_CTRL_Test extends OW_ActionController
 {
-    public function init()
+/*    public function init()
     {
 
         OW::getDocument()->addStyleSheet( OW::getPluginManager()->getPlugin('kosplugin')->getStaticCssUrl().'kosplugin.css' );
 
-    }
+    }*/
     public function index()
     {
-     //   OW::getDocument()->addStyleSheet( OW::getPluginManager()->getPlugin('kosplugin')->getStaticCssUrl().'kosplugin.css' );
+        $language = OW::getLanguage();
+        //$router = OW::getRouter();
+
+        OW::getDocument()->addStyleSheet( OW::getPluginManager()->getPlugin('kosplugin')->getStaticCssUrl().'kosplugin.css' );
+
+        OW::getDocument()->setTitle($language->text("kosplugin", "index_page_title"));
+        OW::getDocument()->setDescription($language->text("kosplugin", "index_page_description"));
+        OW::getDocument()->setHeading($language->text("kosplugin", "index_page_heading"));
     }
 }
